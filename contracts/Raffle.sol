432b41b6e4a279abc64eed419f81e2a9226988a7
Elias Velardez <eliasvelardez38@gmail.com>
2022-11-07 20:54:11 -0300

    feat: create raffle contract with enterRaffle method

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -2,4 +2,35 @@
 
 pragma solidity ^0.8.8;
 
-contract Raffle {}
+error Raffle__NotEnoughETH();
+
+contract Raffle {
+	address payable[] private s_participants;
+	address private i_latestWinner;
+	uint256 private immutable i_entranceFee;
+
+	event RaffleEntered(address indexed participant);
+
+	constructor(uint256 entranceFee) {
+		i_entranceFee = entranceFee;
+	}
+
+	function enterRaffe() external payable {
+		if (msg.value < i_entranceFee) revert Raffle__NotEnoughETH();
+		s_participants.push(payable(msg.sender));
+		emit RaffleEntered(msg.sender);
+	}
+
+	// getters
+	function getParticipant(uint256 index) public view returns (address) {
+		return s_participants[index];
+	}
+
+	function getEntranceFee() public view returns (uint256) {
+		return i_entranceFee;
+	}
+
+	function getLatestWinner() public view returns (address) {
+		return i_latestWinner;
+	}
+}
